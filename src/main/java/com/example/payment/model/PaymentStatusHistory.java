2f33f879a2e8164c1a1ad2af392f06b572412b59
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-10-17 01:00:36 +0700

    Feature/sm 0032 (#3)
    * feature/SM-0032
    
    * feature/SM-0032

--- src/main/java/com/example/payment/model/PaymentStatusHistory.java
+++ src/main/java/com/example/payment/model/PaymentStatusHistory.java
@@ -1,18 +1,18 @@
 package com.example.payment.model;
 
 import com.example.springbootmicroservicesframework.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Table(name = "payment_status_history")
 @Entity
