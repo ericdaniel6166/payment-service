168c2779e01aba251c5d6cf6586dd2bdbba18f8d
eric6166 <ericnguyen6166@gmail.com>
2023-10-17 00:56:15 +0700

    feature/SM-0032

--- src/main/java/com/example/payment/PaymentServiceApplication.java
+++ src/main/java/com/example/payment/PaymentServiceApplication.java
@@ -2,14 +2,12 @@ package com.example.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example"})
-@EnableEurekaClient
-@EnableFeignClients
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
